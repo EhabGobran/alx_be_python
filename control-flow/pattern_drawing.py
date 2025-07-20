e24657ab371135a653d323019acf2e23f6779d72
Ehab <ehapgobarn@gmail.com>
2025-07-20 17:27:02 +0300

    All Tasks 9

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,6 @@
 user_input = int(input("Enter the size of the pattern: "))
-for i in range(1,user_input+1):
-    if i != 1:
-        print()
+i = 1
+while i < user_input + 1:
     for j in range(1,user_input+1):
         print('*',end="")
+    print()
