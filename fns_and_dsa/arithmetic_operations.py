120826412f9452934213a6fb71109ea1f9b9b4f0
Ehab <ehapgobarn@gmail.com>
2025-07-26 23:58:26 +0300

    All Tasks 12

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,10 +10,11 @@ def perform_operation(num1, num2, operation):
             res = num1 * num2
         
         case "divide":
-            if num2 == 0:
+            if num2 != 0:
+                res = num1 / num2
+            elif num2 == 0:
                 return "logical erroe dividing by zero"
                 
-            res = num1 / num2
     return res
             
 
