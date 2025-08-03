35ff687b650f36ab011542acb0f4b9a95f005e06
Ehab <ehapgobarn@gmail.com>
2025-08-04 00:59:37 +0300

    All Tasks 20

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,7 +11,7 @@ class BankAccount:
             return True
         return False
     
-    def display_balance(self,amount):
-        print(f"Current Balance: ${amount}")
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
 
     
